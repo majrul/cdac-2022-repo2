e07eed30020699fae33e3a499cebeb54d19877d9
Majrul Ansari <majrul@gmail.com>
2022-12-21 09:48:40 +0530

    additonal code to display the date

--- MyApplication/src/com/cdac/HelloWorld.java
+++ MyApplication/src/com/cdac/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.cdac;
 
+import java.util.Date;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Date d = new Date();
+		System.out.println(d);
 		
 	}
 }
